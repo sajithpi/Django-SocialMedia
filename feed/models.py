8ed4c1c5675764747b9cbe9bb69cdcb89f9b5a42
SAJITH P I <sajithpalamanna@gmail.com>
2021-12-09 16:21:39 +0530

    Adding author details to post templates

--- feed/models.py
+++ feed/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 from sorl.thumbnail import ImageField
 # Create your models here.
 
@@ -6,6 +8,11 @@ class Post(models.Model):
     text = models.CharField(max_length=255)
     date_time = models.DateTimeField(auto_now=True)
     photo = ImageField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+
+    )
 
 
     def __str__(self):
