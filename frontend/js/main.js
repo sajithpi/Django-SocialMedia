c9098b7f5d83d42ae1380af5d3f4cf117ea1bbc4
SAJITH P I <sajithpalamanna@gmail.com>
2021-12-09 21:40:44 +0530

    Added javascript and toggle function

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,35 @@
-console.log("Hello world")
+$.ajaxSetup({
+    beforeSend: function beforeSend(xhr, settings) {
+        function getCookie(name) {
+            let cookieValue = null;
+
+
+            if (document.cookie && document.cookie !== '') {
+                const cookies = document.cookie.split(';');
+
+                for (let i = 0; i < cookies.length; i += 1) {
+                    const cookie = jQuery.trim(cookies[i]);
+
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+
+            return cookieValue;
+        }
+
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+        }
+    },
+});
+
+$(document).on("click",".js-toggle-model",function(e){
+    e.preventDefault()
+    console.log("I was clicked")
+    $(".js-model").toggleClass("hidden")
+})
