6ae6ef91790af8d8727ef52ebdf9b2ba03690e41
SAJITH P I <sajithpalamanna@gmail.com>
2022-03-24 17:16:16 +0530

    Removed runtime.txt

--- til/settings.py
+++ til/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['socialbook79.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
