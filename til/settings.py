51f052e95957b7dc8b0aecf5f3f7905f4d07f866
SAJITH P I <sajithpalamanna@gmail.com>
2022-03-28 18:20:14 +0530

    Final stage

--- til/settings.py
+++ til/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = []
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
